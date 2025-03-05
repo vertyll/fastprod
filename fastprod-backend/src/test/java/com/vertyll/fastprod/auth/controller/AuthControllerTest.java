1de27a7d747d842bdadaef7214da3c5f47d51053
Mikołaj Gawron <gawrmiko@gmail.com>
2025-03-05 23:22:07 +0100

    chore: remove unused dependencies

--- fastprod-backend/src/test/java/com/vertyll/fastprod/auth/controller/AuthControllerTest.java
+++ fastprod-backend/src/test/java/com/vertyll/fastprod/auth/controller/AuthControllerTest.java
@@ -18,7 +18,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import org.hamcrest.Matchers;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
