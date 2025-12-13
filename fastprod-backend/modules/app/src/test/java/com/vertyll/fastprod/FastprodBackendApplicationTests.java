2183ef7752627bf468d92429205f9ec8f67c30dd
Mikołaj Gawron <gawrmiko@gmail.com>
2025-12-13 10:56:25 +0100

    test: change test db

--- fastprod-backend/modules/app/src/test/java/com/vertyll/fastprod/FastprodBackendApplicationTests.java
+++ fastprod-backend/modules/app/src/test/java/com/vertyll/fastprod/FastprodBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.vertyll.fastprod;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FastprodBackendApplicationTests {
 
     @Test
